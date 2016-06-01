d73e4a1671b55b06f056de3a4ba9c7b3453a4736
Raido Pahtma <raidopahtma@gmail.com>
2016-06-01 12:24:24 +0300

    Improved status monitoring - output/input/uptime and symlinks.

--- tail_subsmanager.py
+++ tail_subsmanager.py
@@ -1,70 +1,161 @@
 #!/usr/bin/env python2
-
-"""Tool for monitoring subscription manager state from logfiles"""
-
-__author__ = "Raido Pahtma"
-__license__ = "MIT"
-
+"""tail_subsmanager.py: Tool for monitoring subscription manager state from logfiles"""
+import calendar
 import sys
 import os
 import time
 import re
+import datetime
+
+__author__ = "Raido Pahtma"
+__license__ = "MIT"
 
 INTERVAL = 0.1
+
 
 class AddressStatus(object):
 
     def __init__(self, addr=0):
         self.addr = addr
+        self.boot = None
 
-    def parse(self, statusline):
+    def parse(self, statusline, timestamp):
         # 2015-08-03T07:27:10.20Z 'I|binf:  22|TOS_NODE_ID EEA2 GUID 01A2EE0E 1500001D'
         m = re.search("TOS_NODE_ID ([0-9A-F]*) GUID ([ 0-9A-F]*).*", statusline)
         if m is not None:
             self.addr = int(m.group(1), 16)
+            try:
+                self.boot = calendar.timegm(datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f").timetuple())
+            except ValueError:
+                self.boot = None
             return True
 
     def __str__(self):
-        return "%04X" % (self.addr)
+        if self.boot is None:
+            uptime = None
+        else:
+            uptime = int(time.time() - self.boot)
+        return "%04X (uptime %s)" % (self.addr, uptime)
+
+
+class OutputStatus(object):
+
+    def __init__(self, output=None):
+        self.output = output
+
+    def parse(self, statusline, timestamp):
+        # 2016-05-31 08:07:07.933: I | dclc: 117 | output[100]
+        m = re.search("output\[\s*(-?[0-9]+)\].*", statusline)
+        if m is not None:
+            self.output = int(m.group(1), 10)
+            return True
+
+    def __str__(self):
+        if self.output is None:
+            return "---"
+        return "%3d" % (self.output)
+
+
+class InputStatus(object):
+
+    def __init__(self, input=None):
+        self.input = input
+
+    def parse(self, statusline, timestamp):
+        # 2016-05-31 08:07:07.933: I | dclc: 117 | output[100]
+        m = re.search("input\[\s*(-?[0-9]+)\].*", statusline)
+        if m is not None:
+            self.input = int(m.group(1), 10)
+            return True
+
+    def __str__(self):
+        if self.input is None:
+            return "---"
+        return "%3d" % (self.input)
 
 
 class ManagerStatus(object):
 
     def __init__(self, index=None):
         self.index = index
-        self.addr = self.cid = self.slot = self.priority = self.max_timeout = self.start = self.maintenance = self.contact = None
+        self.priority = self.len = self.status = self.stored = self.max_timeout = self.start = None
 
-    def parse(self, statusline):
-        # 2015-07-31T14:21:46.93Z 'D|smng: 594|[01] --'
-        m = re.search("\[([0-9]*)\] --.*", statusline)
+    def parse(self, statusline, timestamp):
+        # 2015-07-31T14:21:46.93Z 'D|sbslog: 594|[01] --'
+        m = re.search("s\[([0-9]*)\] --.*", statusline)
         if m is not None:
             self.index = int(m.group(1))
             return True
 
-        # 2015-07-31T14:21:46.93Z 'D|smng: 589|[00] F802 cid 4 s0 p0 637/638/637'
-        m = re.search("\[([0-9]*)\] ([0-9A-F]*) cid ([0-9]+) s([0-9]+) p([0-9]+) t([0-9]+) ([0-9]+)/([0-9]+)/([0-9]+).*", statusline)
+        # 2016-04-07 13:00:57.468 : D|  sbslog:  23|s[00] p0 l33 (1|0) 14/3600
+        m = re.search("s\[([0-9]*)\] p([0-9]+) l([0-9]+) \(([0-9]+)\|([0-9]+)\) ([0-9]+)/([0-9]+).*", statusline)
         if m is not None:
             # ('00', 'F802', '4', '0', '0', '637', '638', '637')
             self.index = int(m.group(1))
-            self.addr = int(m.group(2), 16)
-            self.cid = int(m.group(3))
-            self.slot = int(m.group(4))
-            self.priority = int(m.group(5))
-            self.max_timeout = int(m.group(6))
-            self.start = int(m.group(7))
-            self.maintenance = int(m.group(8))
-            self.contact = int(m.group(9))
+            self.priority = int(m.group(2))
+            self.len = int(m.group(3))
+            self.status = int(m.group(4))
+            self.stored = int(m.group(5))
+            self.start = int(m.group(6))
+            self.max_timeout = int(m.group(7))
+
+            if self.max_timeout == 0xFFFFFFFF:
+                self.max_timeout = "never"
+
             return True
 
         return False
 
     def __str__(self):
         if self.index is None:
-            return "[#%s#]_addr|_cid_|ss|pp|_timeout__|_start____|_maint____|_contact__|" % ("")
-        elif self.addr is None:
-            return "[%02u]     |     |  |  |          |          |          |          |" % (self.index)
+            return "[ l]len|pp|s|f|_start____|_timeout__|"
+        elif self.len is None:
+            return "[%02u]   |  | | |          |          |" % (self.index)
         else:
-            return "[%02u] %04X|%5u|%2u|%2u|%10u|%10u|%10u|%10u|" % (self.index, self.addr, self.cid, self.slot, self.priority, self.max_timeout, self.start, self.maintenance, self.contact)
+            return "[%02u]%3u|%2u|%u|%u|%10u|%10s|" % (self.index, self.len, self.priority, self.status, self.stored, self.start, self.max_timeout)
+
+
+class StreamStatus(object):
+
+    def __init__(self, index=None):
+        self.index = index
+        self.lid = 0xFF
+        self.addr = self.cid = self.slot = self.status = self.stored = self.contact = self.maintenance = None
+
+    def parse(self, statusline, timestamp):
+        # 2015-07-31T14:21:46.93Z 'D|sbslog: 594|[01] --'
+        m = re.search("t\[([0-9]*)\] --.*", statusline)
+        if m is not None:
+            self.index = int(m.group(1))
+            return True
+
+        # 2016-04-07 14:55:47.107 : D|  sbslog:  35|t[00|00] BDE8:834e(0)(1|0) 14/14
+        m = re.search("t\[([0-9]+)\|([0-9]+)\] ([0-9A-F]+):([0-9a-f]+)\(([0-9]+)\)\(([0-9]+)\|([0-9]+)\) ([0-9]+)/([0-9]+).*", statusline)
+        if m is not None:
+            print "match", m.groups()
+            # ('00', 'F802', '4', '0', '0', '637', '638', '637')
+            self.index = int(m.group(1))
+            self.lid = int(m.group(2))
+            self.addr = int(m.group(3), 16)
+            self.cid = int(m.group(4), 16)
+            self.slot = int(m.group(5))
+            self.status = int(m.group(6))
+            self.stored = int(m.group(7))
+            self.contact = int(m.group(8))
+            self.maintenance = int(m.group(9))
+            return True
+        else:
+            print "bad", statusline
+
+        return False
+
+    def __str__(self):
+        if self.index is None:
+            return "[ s| l]|addr|__cid___|ss|s|f|_contact__|_maint____|"
+        elif self.addr is None:
+            return "[%02u|%s%s]|    |        |  | | |          |          |" % (self.index, " ", " ")
+        else:
+            return "[%02u|%02u]|%04X|%8x|%2u|%u|%u|%10u|%10u|" % (self.index, self.lid, self.addr, self.cid, self.slot, self.status, self.stored, self.contact, self.maintenance)
 
 
 class MiddlewareStatus(object):
@@ -73,7 +164,7 @@ class MiddlewareStatus(object):
         self.index = index
         self.addr = self.state = self.cid = self.priority = self.last_broadcast = self.max_timeout = self.providers = None
 
-    def parse(self, statusline):
+    def parse(self, statusline, timestamp):
         m = re.search("\[([0-9]*)\] --.*", statusline)
         if m is not None:
             self.index = int(m.group(1))
@@ -89,17 +180,21 @@ class MiddlewareStatus(object):
             self.last_broadcast = int(m.group(5))
             self.max_timeout = int(m.group(6))
             self.providers = int(m.group(7))
+
+            if self.max_timeout == 0xFFFFFFFF:
+                self.max_timeout = "never"
+
             return True
 
         return False
 
     def __str__(self):
         if self.index is None:
-            return "[#%s#]_st|_cid_|pp|_broadcast|_timeout__|pc|" % ("")
+            return "[mw]_st|__cid___|pp|_broadcast|_timeout__|pc|_last_data|"
         elif self.state is None:
-            return "[%02u]   |     |  |          |          |  |" % (self.index)
+            return "[%02u]   |        |  |          |          |  |          |" % (self.index)
         else:
-            return "[%02u]%3u|%5u|%2u|%10u|%10u|%2u|" % (self.index, self.state, self.cid, self.priority, self.last_broadcast, self.max_timeout, self.providers)
+            return "[%02u]%3u|%8x|%2u|%10u|%10s|%2u|---TODO---|" % (self.index, self.state, self.cid, self.priority, self.last_broadcast, self.max_timeout, self.providers)
 
 
 class SchedulerStatus(object):
@@ -108,7 +203,7 @@ class SchedulerStatus(object):
         self.index = index
         self.addr = self.lid = self.state = None
 
-    def parse(self, statusline):
+    def parse(self, statusline, timestamp):
         m = re.search("\[([0-9]*)\]\(--\).*", statusline)
         if m is not None:
             self.index = int(m.group(1))
@@ -126,25 +221,46 @@ class SchedulerStatus(object):
 
     def __str__(self):
         if self.index is None:
-            return "[#%s#]lid|_st|" % ("")
+            return "[ a| l|_st|"
         elif self.lid is None:
-            return "[%02u][  ]   |" % (self.index)
+            return "[%02u|  ]   |" % (self.index)
         else:
-            return "[%02u][%02u]%3u|" % (self.index, self.lid, self.state)
+            return "[%02u|%02u]%3u|" % (self.index, self.lid, self.state)
 
 
-def tailfile(file):
+def tail_file(file_path, seek=0):
+    logfile = open(file_path, "r")
+    logfile.seek(seek)
+    actual_path = None
+    if os.path.islink(file_path):
+        actual_path = os.readlink(file_path)
+
     while True:
-        where = file.tell()
-        line = file.readline()
+        if actual_path is not None:
+            new_path = os.readlink(file_path)
+            if new_path != actual_path:
+                actual_path = new_path
+                logfile.close()
+                logfile = open(file_path)
+                print "opened {}".format(actual_path)
+
+        where = logfile.tell()
+        line = logfile.readline()
         if line:
             line = line.lstrip().rstrip()
-            m = re.search("(.*) '[DIWE]\|(.*):[ 0-9]*\|(.*)", line)
+            m = re.search("(.*):\s*[DIWE]\|(.*):[ 0-9]*\|(.*)", line)
             if m is not None:
                 timestamp, module, logline = m.groups()
+                module = module.strip()
+                timestamp = timestamp.strip()
 
-                if module.startswith("smng"):
-                    s = ManagerStatus()
+                if module.startswith("sbslog"):
+                    if logline.startswith("s"):
+                        s = ManagerStatus()
+                    elif logline.startswith("t"):
+                        s = StreamStatus()
+                    else:
+                        continue
                 elif module.startswith("mddl"):
                     s = MiddlewareStatus()
                 elif module.startswith("amdl"):
@@ -152,47 +268,68 @@ def tailfile(file):
                 elif module.startswith("binf"):
                     s = AddressStatus()
                 else:
-                    continue
+                    if logline.find("output") >= 0:
+                        s = OutputStatus()
+                    elif logline.find("input") >= 0:
+                        s = InputStatus()
+                    else:
+                        # print "useless", line
+                        continue
 
-                if s.parse(logline):
+                if s.parse(logline, timestamp):
                     yield s
+            # else:
+            #    print "bad", line
         else:
             time.sleep(INTERVAL)
-            file.seek(where)
+            logfile.seek(where)
 
 
-if __name__ == "__main__":
-
+def main():
     from argparse import ArgumentParser
     parser = ArgumentParser(description="Statusparser")
     parser.add_argument("filename")
     parser.add_argument("--old", action="store_true")
     args = parser.parse_args()
 
+    logfile = None
     try:
         if args.old:
-            filesize = 0
+            file_size = 0
         else:
-            filesize = os.stat(args.filename)[6]
-
-        file = open(args.filename, "r")
-        file.seek(filesize)
+            file_size = os.stat(args.filename)[6]
 
         addr = AddressStatus()
         managermap = {0: ManagerStatus(0)}
+        streammap = {0: StreamStatus(0)}
         middlewaremap = {0: MiddlewareStatus(0)}
         schedulermap = {0: SchedulerStatus(0)}
-        for status in tailfile(file):
+        output = OutputStatus()
+        input = InputStatus()
+        for status in tail_file(args.filename, seek=file_size):
             if isinstance(status, ManagerStatus):
                 managermap[status.index] = status
+            if isinstance(status, StreamStatus):
+                streammap[status.index] = status
             elif isinstance(status, MiddlewareStatus):
                 middlewaremap[status.index] = status
             elif isinstance(status, SchedulerStatus):
                 schedulermap[status.index] = status
             elif isinstance(status, AddressStatus):
                 addr = status
+                managermap = {0: ManagerStatus(0)}
+                streammap = {0: StreamStatus(0)}
+                # not reset because boot will print all slots
+                # middlewaremap = {0: MiddlewareStatus(0)}
+                # schedulermap = {0: SchedulerStatus(0)}
+                output = OutputStatus()
+                input = InputStatus()
+            elif isinstance(status, OutputStatus):
+                output = status
+            elif isinstance(status, InputStatus):
+                input = status
 
-            print "\033c" # clear screen
+            print "\033c"  # clear screen
             print addr
             print
 
@@ -201,6 +338,13 @@ if __name__ == "__main__":
                 if i not in managermap:
                     managermap[i] = ManagerStatus(i)
                 print managermap[i]
+
+            print
+            print StreamStatus()
+            for i in xrange(0, max(streammap.iterkeys())+1):
+                if i not in streammap:
+                    streammap[i] = StreamStatus(i)
+                print streammap[i]
 
             print
             print MiddlewareStatus()
@@ -216,7 +360,16 @@ if __name__ == "__main__":
                     schedulermap[i] = SchedulerStatus(i)
                 print schedulermap[i]
 
+            print
+            print "Output {}".format(output)
+            print "Input  {}".format(input)
+
     except KeyboardInterrupt:
         print "interrupted"
-        file.close()
+        if logfile is not None:
+            logfile.close()
         sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    main()
