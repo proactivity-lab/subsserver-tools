2f9852fb8a66b68e39c1e1bf23876c668b6a84ae
Raido Pahtma <raidopahtma@gmail.com>
2016-06-17 15:03:25 +0300

    Handle new logs.

--- tail_subsmanager.py
+++ tail_subsmanager.py
@@ -81,7 +81,7 @@ class ManagerStatus(object):
 
     def __init__(self, index=None):
         self.index = index
-        self.priority = self.len = self.status = self.stored = self.max_timeout = self.start = None
+        self.priority = self.len = self.status = self.stored = self.max_timeout = self.start = self.streams = None
 
     def parse(self, statusline, timestamp):
         # 2015-07-31T14:21:46.93Z 'D|sbslog: 594|[01] --'
@@ -91,7 +91,7 @@ class ManagerStatus(object):
             return True
 
         # 2016-04-07 13:00:57.468 : D|  sbslog:  23|s[00] p0 l33 (1|0) 14/3600
-        m = re.search("s\[([0-9]*)\] p([0-9]+) l([0-9]+) \(([0-9]+)\|([0-9]+)\) ([0-9]+)/([0-9]+).*", statusline)
+        m = re.search("s\[([0-9]*)\] p([0-9]+) l([0-9]+) \(([0-9]+)\|([0-9]+)\) ([0-9]+)/([0-9]+) \(([0-9]+)\).*", statusline)
         if m is not None:
             # ('00', 'F802', '4', '0', '0', '637', '638', '637')
             self.index = int(m.group(1))
@@ -101,6 +101,7 @@ class ManagerStatus(object):
             self.stored = int(m.group(5))
             self.start = int(m.group(6))
             self.max_timeout = int(m.group(7))
+            self.streams = int(m.group(8))
 
             if self.max_timeout == 0xFFFFFFFF:
                 self.max_timeout = "never"
@@ -111,11 +112,11 @@ class ManagerStatus(object):
 
     def __str__(self):
         if self.index is None:
-            return "[ l]len|pp|s|f|_start____|_timeout__|"
+            return "[ l]len|pp|s|f|_start____|_timeout__|strms|"
         elif self.len is None:
-            return "[%02u]   |  | | |          |          |" % (self.index)
+            return "[%02u]   |  | | |          |          |     |" % (self.index)
         else:
-            return "[%02u]%3u|%2u|%u|%u|%10u|%10s|" % (self.index, self.len, self.priority, self.status, self.stored, self.start, self.max_timeout)
+            return "[%02u]%3u|%2u|%u|%u|%10u|%10s|%5u|" % (self.index, self.len, self.priority, self.status, self.stored, self.start, self.max_timeout, self.streams)
 
 
 class StreamStatus(object):
@@ -123,7 +124,7 @@ class StreamStatus(object):
     def __init__(self, index=None):
         self.index = index
         self.lid = 0xFF
-        self.addr = self.cid = self.slot = self.status = self.stored = self.contact = self.maintenance = None
+        self.addr = self.cid = self.slot = self.status = self.stored = self.contact = self.maintenance = self.data_out = None
 
     def parse(self, statusline, timestamp):
         # 2015-07-31T14:21:46.93Z 'D|sbslog: 594|[01] --'
@@ -133,7 +134,7 @@ class StreamStatus(object):
             return True
 
         # 2016-04-07 14:55:47.107 : D|  sbslog:  35|t[00|00] BDE8:834e(0)(1|0) 14/14
-        m = re.search("t\[([0-9]+)\|([0-9]+)\] ([0-9A-F]+):([0-9a-f]+)\(([0-9]+)\)\(([0-9]+)\|([0-9]+)\) ([0-9]+)/([0-9]+).*", statusline)
+        m = re.search("t\[([0-9]+)\|([0-9]+)\] ([0-9A-F]+):([0-9a-f]+)\(([0-9]+)\)\(([0-9]+)\|([0-9]+)\) ([0-9]+)/([0-9]+)/([0-9]+).*", statusline)
         if m is not None:
             print "match", m.groups()
             # ('00', 'F802', '4', '0', '0', '637', '638', '637')
@@ -146,6 +147,7 @@ class StreamStatus(object):
             self.stored = int(m.group(7))
             self.contact = int(m.group(8))
             self.maintenance = int(m.group(9))
+            self.data_out = int(m.group(10))
             return True
         else:
             print "bad", statusline
@@ -154,11 +156,11 @@ class StreamStatus(object):
 
     def __str__(self):
         if self.index is None:
-            return "[ s| l]|addr|__cid___|ss|s|f|_contact__|_maint____|"
+            return "[ s| l]|addr|__cid___|ss|s|f|_contact__|_maint____|_data_____|"
         elif self.addr is None:
-            return "[%02u|%s%s]|    |        |  | | |          |          |" % (self.index, " ", " ")
+            return "[%02u|%s%s]|    |        |  | | |          |          |          |" % (self.index, " ", " ")
         else:
-            return "[%02u|%02u]|%04X|%8x|%2u|%u|%u|%10u|%10u|" % (self.index, self.lid, self.addr, self.cid, self.slot, self.status, self.stored, self.contact, self.maintenance)
+            return "[%02u|%02u]|%04X|%8x|%2u|%u|%u|%10u|%10u|%10u|" % (self.index, self.lid, self.addr, self.cid, self.slot, self.status, self.stored, self.contact, self.maintenance, self.data_out)
 
 
 class MiddlewareStatus(object):
